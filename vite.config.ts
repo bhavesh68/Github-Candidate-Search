d5d0255e2532d3baa5315872a23ed9531274e144
bhavesh68 <bhaveshkhatwani999@gmail.com>
2025-01-28 23:17:47 -0500

    authorized code updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: parseInt(process.env.PORT) || 4153,
+    port: 4153,
     allowedHosts: ["https://github-candidate-search-wxzs.onrender.com"]
   }
 });
